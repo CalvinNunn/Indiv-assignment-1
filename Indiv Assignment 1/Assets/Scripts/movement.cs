26d3f519b6385d5a99b22c5dafd0cfdbdee6d7ca
Calvin Nunn <calvin.nunn@ontariotechu.net>
2019-09-27 14:09:05 -0400

    Initial commit

---
+++ Indiv Assignment 1/Assets/Scripts/movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) {
+            transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + 1);
+        }
+        if (Input.GetKeyDown(KeyCode.A)) {
+            transform.position = new Vector3(this.transform.position.x - 1, this.transform.position.y, this.transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.S)) {
+            transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 1);
+        }
+        if (Input.GetKeyDown(KeyCode.D)) {
+            transform.position = new Vector3(this.transform.position.x + 1, this.transform.position.y, this.transform.position.z);
+        }
+    }
+}
