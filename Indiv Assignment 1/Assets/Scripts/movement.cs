c0f5fb2703abab82175ce9341d95bbd0af64598d
Calvin Nunn <calvin.nunn@ontariotechu.net>
2019-09-27 16:01:51 -0400

    Final commit

--- Indiv Assignment 1/Assets/Scripts/movement.cs
+++ Indiv Assignment 1/Assets/Scripts/movement.cs
@@ -8,17 +8,17 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)) {
-            transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + 1);
+        if (Input.GetKey(KeyCode.W)) {
+            transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + 0.1f);
         }
-        if (Input.GetKeyDown(KeyCode.A)) {
-            transform.position = new Vector3(this.transform.position.x - 1, this.transform.position.y, this.transform.position.z);
+        if (Input.GetKey(KeyCode.A)) {
+            transform.position = new Vector3(this.transform.position.x - 0.1f, this.transform.position.y, this.transform.position.z);
         }
-        if (Input.GetKeyDown(KeyCode.S)) {
-            transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 1);
+        if (Input.GetKey(KeyCode.S)) {
+            transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 0.1f);
         }
-        if (Input.GetKeyDown(KeyCode.D)) {
-            transform.position = new Vector3(this.transform.position.x + 1, this.transform.position.y, this.transform.position.z);
+        if (Input.GetKey(KeyCode.D)) {
+            transform.position = new Vector3(this.transform.position.x + 0.1f, this.transform.position.y, this.transform.position.z);
         }
     }
 }
---
+++ Indiv Assignment 1/Assets/Scripts/saveload.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class saveload : MonoBehaviour
+{
+
+    public GameObject positionObject;
+
+    const string DLL_NAME = "Ass1_DLL";
+
+    [DllImport(DLL_NAME)]
+    private static extern void OutputFile(float x, float y, float z);
+
+    [DllImport(DLL_NAME)]
+    private static extern float ImportFileX();
+
+    [DllImport(DLL_NAME)]
+    private static extern float ImportFileY();
+
+    [DllImport(DLL_NAME)]
+    private static extern float ImportFileZ();
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            OutputFile(positionObject.transform.position.x, positionObject.transform.position.y, positionObject.transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            positionObject.transform.position = new Vector3(ImportFileX(), ImportFileY(), ImportFileZ());
+        }
+    }
+}
